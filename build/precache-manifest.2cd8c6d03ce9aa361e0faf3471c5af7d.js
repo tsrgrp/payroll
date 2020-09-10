4a60e1b243683d2c70bfd6f79ac2478ee8b860d2
tsrgrp <tsrgroup.pltd@gmail.com>
2020-09-11 11:44:06 +1200

    Add files via upload

---
+++ build/precache-manifest.2cd8c6d03ce9aa361e0faf3471c5af7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5553f955e1313420a63d5a7823f1d80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64fddd7ffc79663681b4",
+    "url": "/static/css/main.56b0d637.chunk.css"
+  },
+  {
+    "revision": "823824c840262864cee5",
+    "url": "/static/js/2.7c98c920.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.7c98c920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fddd7ffc79663681b4",
+    "url": "/static/js/main.6221ba77.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
